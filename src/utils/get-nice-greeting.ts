acd8b522a6e115ce3cfe1502fc9d19e5f63f221d
soleil <46466811+nocturnll@users.noreply.github.com>
2023-03-08 11:40:58 -0800

    fix: typo in touchbar copy (#62)

--- src/utils/get-nice-greeting.ts
+++ src/utils/get-nice-greeting.ts
@@ -7,17 +7,25 @@ import { getRandomNumber } from './random';
  * @returns
  */
 export function getNiceGreeting() {
-  const greetings = [
+  const today = new Date();
+  let greetings = [''];
+  if (today.getMonth() === 1 && today.getDate() === 12) {
+    greetings = ['🎂 Happy birthday, Slack'];
+  } else {
+  greetings = [
     '👋 Thanks for checking out logs',
     '💖 You got this',
     `💖 We think you're great`,
-    '🤘 Your rock, we know it',
+    '🤘 You rock, we know it',
     '🙇‍♀️ Thanks for trying out Sleuth',
     '🐕 Go pet a dog today',
     '🐈 Go pet a cat today',
     '💧 Stay hydrated',
-    '🙇‍♂️ Many thanks from the desktop team'
+    '🙇‍♂️ Many thanks from the desktop team',
+    '🐙 The world is your oyster',
+    '🌅 Have a wonderful day'
   ];
+}
 
   const min = 0;
   const max = greetings.length;
