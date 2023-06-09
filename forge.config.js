7cf93c1ca9bb28b1d65ad212049a2e26368d5e6f
Erick Zhao <ezhao@slack-corp.com>
2023-06-09 11:07:50 -0700

    test without timestamp server

--- forge.config.js
+++ forge.config.js
@@ -1,51 +1,13 @@
 /* eslint-disable */
 //@ts-check
-const fs = require("fs-extra");
-const os = require("os");
 const path = require("path");
 
 const iconDir = path.join(__dirname, "static/img");
 const version = require("./package.json").version;
 
-const http = require("http");
-const httpProxy = require("http-proxy");
-
-let server;
-const PORT = 37492;
-
 const options = {
   hooks: {
     generateAssets: require("./tools/generateAssets"),
-    preMake: async () => {
-      let server = null;
-      let dir = null;
-      try {
-        const timestampProxiedProxy = httpProxy.createProxyServer({});
-
-        server = http.createServer((req, res) => {
-          return timestampProxiedProxy.web(req, res, {
-            target: "http://timestamp.digicert.com",
-          });
-        });
-
-        await new Promise((resolve) => {
-          server.listen(PORT, () => {
-            resolve(null);
-          });
-          console.log(`server listening on port ${PORT}`);
-        });
-
-        dir = await fs.mkdtemp(
-          path.resolve(os.tmpdir(), "slack-builder-folder-")
-        );
-      } finally {
-        if (dir) await fs.remove(dir);
-      }
-    },
-    postMake: async () => {
-      server.close();
-      console.log(`server closing`);
-    },
   },
   packagerConfig: {
     name: "Sleuth",
@@ -90,7 +52,7 @@ const options = {
           noMsi: true,
           setupExe: `sleuth-${version}-${arch}-setup.exe`,
           setupIcon: path.resolve(iconDir, "sleuth-icon.ico"),
-          signWithParams: `/v /debug /a /sm /fd sha256 /sha1 ${certThumbPrint} /tr http://localhost:${PORT} /td sha256`,
+          signWithParams: `/as /sm /fd sha256 /sha1 ${certThumbPrint}`,
         };
       },
     },
