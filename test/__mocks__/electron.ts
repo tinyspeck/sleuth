869d3a30e82e21d53de43988bf617d351ac99cca
Johan Delao <jdelao@slack-corp.com>
2023-06-30 12:53:31 -0700

    fixed issues with tests

--- test/__mocks__/electron.ts
+++ test/__mocks__/electron.ts
@@ -1,18 +1,6 @@
-import path from 'path';
-
 const ipcRenderer = {
   send: jest.fn(),
   invoke: jest.fn(),
-};
-
-const app = {
-  getPath(target: string) {
-    if (target === 'downloads') {
-      return path.join(__dirname, '../static/');
-    }
-
-    return __dirname;
-  }
 };
 
 const shell = {
