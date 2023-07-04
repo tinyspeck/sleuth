a89eb5cfb2e6c90d373c215089db19bc74426be9
JohanDelao <52749324+JohanDelao@users.noreply.github.com>
2023-07-04 10:32:10 -0700

    fix: disable remote module (#92)

--- test/__mocks__/electron.ts
+++ test/__mocks__/electron.ts
@@ -1,26 +1,11 @@
-import path from 'path';
-
 const ipcRenderer = {
   send: jest.fn(),
   invoke: jest.fn(),
-};
-
-const app = {
-  getPath(target: string) {
-    if (target === 'downloads') {
-      return path.join(__dirname, '../static/');
-    }
-
-    return __dirname;
-  }
 };
 
 const shell = {
   trashItem: jest.fn()
 };
 
-const remote = {
-  app
-};
 
-module.exports = { ipcRenderer, remote, shell };
+module.exports = { ipcRenderer, shell };
