3ca7dd72588673c24a51baa622aed69d243ed568
JohanDelao <52749324+JohanDelao@users.noreply.github.com>
2023-07-21 14:32:42 -0700

    chore: remove support for call logs (#110)

--- src/utils/get-first-logfile.ts
+++ src/utils/get-first-logfile.ts
@@ -8,7 +8,6 @@ export function getFirstLogFile(
     if (files.renderer && files.renderer.length > 0) return files.renderer[0];
     if (files.preload && files.preload.length > 0) return files.preload[0];
     if (files.webapp && files.webapp.length > 0) return files.webapp[0];
-    if (files.call && files.call.length > 0) return files.call[0];
     if (files.netlog && files.netlog.length > 0) return files.netlog[0];
     if (files.trace && files.trace.length > 0) return files.netlog[0];
     if (files.installer && files.installer.length > 0)
