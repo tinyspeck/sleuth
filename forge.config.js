2bbef8cb3218fad48b9b8a86eb1c0ada2bde6eca
Johan Delao <jdelao@slack-corp.com>
2023-07-24 15:15:31 -0700

    updated plugin syntax for v6.0.0-beta.68

--- forge.config.js
+++ forge.config.js
@@ -126,7 +126,12 @@ const options = {
       },
     },
   ],
-  plugins: [['@electron-forge/plugin-auto-unpack-natives']],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
 };
 
 module.exports = options;
