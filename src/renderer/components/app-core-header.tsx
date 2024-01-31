7e8f33891c0e2ae6491eb4cec4309f9703c2794f
Erick Zhao <ezhao@slack-corp.com>
2024-01-31 14:23:24 -0800

    downscope

--- src/renderer/components/app-core-header.tsx
+++ src/renderer/components/app-core-header.tsx
@@ -1,9 +1,17 @@
 import { observer } from 'mobx-react';
 import { SleuthState } from '../state/sleuth';
 import React from 'react';
-import { Navbar } from '@blueprintjs/core';
+import {
+  Alignment,
+  Button,
+  ButtonGroup,
+  Divider,
+  Navbar,
+  NavbarGroup,
+} from '@blueprintjs/core';
 
 import { Filter } from './app-core-header-filter';
+import { Bookmarks } from './app-core-header-bookmarks';
 
 export interface AppCoreHeaderProps {
   state: SleuthState;
@@ -12,8 +20,37 @@ export interface AppCoreHeaderProps {
 @observer
 export class AppCoreHeader extends React.Component<AppCoreHeaderProps, object> {
   public render() {
+    const {
+      isSidebarOpen,
+      isDarkMode,
+      isSpotlightOpen,
+      toggleDarkMode,
+      toggleSidebar,
+      toggleSpotlight,
+      reset,
+    } = this.props.state;
+    const sidebarIcon = isSidebarOpen ? 'menu-closed' : 'menu-open';
+
     return (
       <Navbar className="AppHeader">
+        <NavbarGroup align={Alignment.LEFT}>
+          <ButtonGroup>
+            <Button onClick={() => reset(true)} icon="home" />
+            <Button
+              active={!isSidebarOpen}
+              onClick={toggleSidebar}
+              icon={sidebarIcon}
+            />
+            <Button active={isDarkMode} onClick={toggleDarkMode} icon="moon" />
+            <Bookmarks state={this.props.state} />
+            <Button
+              active={isSpotlightOpen}
+              onClick={toggleSpotlight}
+              icon="geosearch"
+            />
+          </ButtonGroup>
+        </NavbarGroup>
+        <Divider />
         <Filter state={this.props.state} />
       </Navbar>
     );
