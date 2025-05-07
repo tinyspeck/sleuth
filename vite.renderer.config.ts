9b7ed484e62bae45afcbad1b83c4ffb07c9aec7c
Erick Zhao <ezhao@slack-corp.com>
2025-05-07 12:16:50 -0700

    disable directive warnings for vite

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -14,6 +14,15 @@ export default defineConfig({
     global: {},
   },
   build: {
-    sourcemap: 'inline',
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Module level directives cause errors when bundled, "use client" in "node_modules/antd/es/color-picker/components/ColorHexInput.js" was ignored.
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        } else {
+          warn(warning);
+        }
+      },
+    },
   },
 });
