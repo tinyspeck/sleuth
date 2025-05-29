fe875a8ed6da135ac6d7a0effa6d104cfdbdefa4
Erick Zhao <ezhao@slack-corp.com>
2025-05-28 22:01:40 -0700

    build: enable HMR with React

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ['decorators-legacy', 'classProperties'],
+        },
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
