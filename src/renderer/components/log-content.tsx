a8305a9186ef6d9f3be1e6b61d85b69cf19e05d7
Erick Zhao <ezhao@slack-corp.com>
2025-07-31 13:47:34 -0700

    refactor: convert `log-content.tsx` into antd (#231)

--- src/renderer/components/log-content.tsx
+++ src/renderer/components/log-content.tsx
@@ -13,7 +13,6 @@ import { getTypeForFile } from '../../utils/get-file-types';
 import { NetLogView } from './net-log-view';
 import { LogTimeView } from './log-time-view';
 import { DevtoolsView } from './devtools-view';
-import { Navbar } from '@blueprintjs/core';
 import { Filter } from './app-core-header-filter';
 
 export interface LogContentProps {
@@ -71,9 +70,9 @@ export class LogContent extends React.Component<
     if (isLog) {
       return (
         <div className="LogContent" style={{ fontFamily: getFontForCSS(font) }}>
-          <Navbar className="AppHeader">
+          <div className="AppHeader">
             <Filter state={this.props.state} />
-          </Navbar>
+          </div>
           <div
             id="LogTableContainer"
             style={{ height: this.state.tableHeight }}
