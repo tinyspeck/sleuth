f05e0142b42662d17f38331e6085f582f977d855
George Xu <george.xu@slack-corp.com>
2025-08-12 19:31:38 -0700

    remove top nav perms

--- src/renderer/components/perfetto-view.tsx
+++ src/renderer/components/perfetto-view.tsx
@@ -26,7 +26,7 @@ export class PerfettoView extends React.Component<PerfettoViewProps> {
           title="Perfetto UI"
           src={perfettoUrl}
           style={{ width: '100%', height: '100%' }}
-          sandbox="allow-scripts allow-same-origin allow-forms allow-top-navigation"
+          sandbox="allow-scripts allow-same-origin allow-forms"
           allow="clipboard-write"
           loading="lazy"
         />
